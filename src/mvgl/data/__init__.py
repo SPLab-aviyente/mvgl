d525f00aacf0db8d3d94bc3e9e8154897ba80a1d
abdkarr <abdullah.karaaslanli@gmail.com>
2023-11-28 23:01:33 -0500

    Feat: Add view graph generation.

--- src/mvgl/data/__init__.py
+++ src/mvgl/data/__init__.py
@@ -6,9 +6,9 @@ def gen_simulated_data(n_nodes, n_views, n_signals, graph_generator, p, m,
                        perturbation, noise, seed=None):
     rng = np.random.default_rng(seed)
     Gc = graph.gen_consensus_graph(n_nodes, graph_generator, p, m, rng)
-    # Gv = graph.gen_views(n_views, Gc, perturbation)
+    Gv = graph.gen_views(n_views, Gc, perturbation, rng)
 
     # wc_gt, wv_gt = data.vectorize_graphs(Gc, Gv)
 
     # X = data.gen_data(n_signals, Gv, data_filter, data_noise, r+1)
-    return Gc
+    return Gc, Gv
