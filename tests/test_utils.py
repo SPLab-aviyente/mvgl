2451aa801eaf488c37b88ce425f662e30d4c5198
abdkarr <abdullah.karaaslanli@gmail.com>
2023-11-28 23:13:57 -0500

    Test: Add graph vectorization shape testing.

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+from mvgl.data import graph
+from mvgl import utils
+
+def test_graph_vectorization():
+    n_nodes = 50
+    G = graph.gen_consensus_graph(n_nodes, graph_generator="er", p=0.3)
+    
+    w = utils.vectorize_graphs(G)
+
+    # check dimension
+    n_nodes = 50
+    n_pairs = n_nodes*(n_nodes-1)//2
+    assert w.shape == (n_pairs,), "Graph vectorization gives wrong shape."
