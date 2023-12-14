83a9400de1fe8c9d6a09cda82227c591852afabf
abdkarr <abdullah.karaaslanli@gmail.com>
2023-12-14 17:47:48 -0500

    Refactor: Graph vectorization tests.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,7 +1,7 @@
 from mvgl.data import graph
 from mvgl import utils
 
-def test_graph_vectorization():
+def test_graph_vectorization_shape():
     n_nodes = 50
     G = graph.gen_consensus_graph(n_nodes, graph_generator="er", p=0.3)
     
