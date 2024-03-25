73d875e54a6cbdf443df0d2659999cc7e24cf3ca
abdkarr <7543956+abdkarr@users.noreply.github.com>
2024-03-25 13:26:48 -0400

    Feat: Add graph learning utility function rowsum_mat

---
+++ tests/test_graphlearning_utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from mvgl.graphlearning import utils
+
+def test_rowsum_mat_construction():
+    A = np.array([
+        [0, 1, 1, 0, 0, 1],
+        [1, 0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 0, 1, 1, 0, 1],
+        [1, 1, 0, 0, 1, 0]
+    ])
+    n = A.shape[0]
+    a = A[np.triu_indices_from(A, k=1)]
+
+    S = utils.rowsum_mat(n)
+
+    expected = np.sum(A, axis=1)
+    actual = S@a
+
+    msg = "Row-sum matrix returns wrong sums."
+    assert np.sum(np.abs(actual - expected)) < 1e-8, msg
