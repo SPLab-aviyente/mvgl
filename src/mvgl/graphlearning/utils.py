bd4cfef304227d050b91bc201aa09e7150fd5f81
abdkarr <7543956+abdkarr@users.noreply.github.com>
2024-03-25 15:36:51 -0400

    Feat: Add graph learning utility function calc_data_vecs

--- src/mvgl/graphlearning/utils.py
+++ src/mvgl/graphlearning/utils.py
@@ -1,3 +1,6 @@
+"""This module includes utility functions used during learning graph structures.
+"""
+
 import numpy as np
 import networkx as nx
 
@@ -19,7 +22,7 @@ def rowsum_mat(n):
 
     Returns
     -------
-    S : scipy.sparse.csr_array
+    scipy.sparse.csr_array
         Matrix to be used in row-sum calculation.
     """
 
@@ -31,8 +34,7 @@ def rowsum_mat(n):
     return csr_array((np.ones((2*M, )), (rows, cols)), shape=(n, M))
 
 def vectorize_a_graph(G):
-    """Returns the upper triangular part of the adjacency matrix of a graph as 
-    a vector.
+    """Get the upper triangular part of the adjacency of a graph as a vector. 
 
     Parameters
     ----------
@@ -41,7 +43,7 @@ def vectorize_a_graph(G):
 
     Returns
     -------
-    w : np.array
+    np.array
         Output vector as a numpy array.
     """
     n_nodes = G.number_of_nodes()
@@ -49,3 +51,74 @@ def vectorize_a_graph(G):
     w = nx.to_numpy_array(G)[np.triu_indices(n_nodes, k=1)]
 
     return w
+
+def calc_data_vecs(X, normalize=False, S=None):
+    """Calculate data vectors of a set of graph signal data matrices.
+
+    Let :math:`\mathbf{X} \in \mathbb{R}^{n \\times p}` be a data matrix whose
+    columns are graph signals on a graph with :math:`n` nodes. This function
+    calculates the vector :math:`\mathbf{x} \in \mathbb{R}^{n(n-1)/2}`:
+
+    .. math::
+        \mathbf{x} = 2\mathrm{upper}(\mathbf{K}) 
+            - \mathbf{S}^\\top \mathrm{diag}(\mathbf{K}),
+    
+    where :math:`\mathbf{K} = \mathbf{X}\mathbf{X}^\\top`, :math:`\mathbf{S}` is
+    the row-sum matrix, :math:`\mathrm{upper}` is an operator returning upper
+    triangular of part of the input matrix as a vector, and
+    :math:`\mathrm{diag}` is an operator returning the diagonal part of the
+    input matrix as a vector. :math:`\mathbf{x}` can be used to calculate the
+    smoothness of :math:`\mathbf{X}` in a vectorized form: 
+
+    .. math::
+        \mathrm{tr}(\mathbf{X}^\\top\mathbf{\mathbf{L}}\mathbf{X}) = 
+            \mathbf{x}^\\top\mathrm{upper}(\mathbf{L}),
+    where :math:`\mathbf{L}` is the Laplacian of the graph.
+
+    Parameters
+    ----------
+    X : np.array or list of np.array
+        The matrix or a set of matrices whose data vectors will be calculated.
+        If list, data vectors calculated for each matrix independently.
+    normalize : bool, optional
+        Whether to normalize the calculated data vectors before returning them.
+        Normalization is done by dividing the calculated data vector to its
+        infinity norm. This can be helpful when smoothness terms is used with
+        other terms (*e.g.* regularization terms, smoothness terms calculated
+        from other data matrices) such that normalization may ensure scale of
+        the terms are in a similar range. By default False.
+    S : scipy.sparse.csr_array, optional
+        Preconstructed row-sum matrix. If None, it is constructed by
+        :meth:`rowsum_mat`. By default None
+
+    Returns
+    -------
+    np.array or list of np.array
+        Calculated data vectors.
+    """
+
+    # Input check
+    if not isinstance(X, list):
+        X = [X]
+
+    if S is None:
+        S = rowsum_mat(X[0].shape[0])
+    
+    
+    n_views = len(X)
+    data_vecs = []
+    for v in range(n_views):
+        # Calculate data vector for each data matrix in X
+        K = X[v]@X[v].T
+        k = K[np.triu_indices_from(K, k=1)]
+        d = K[np.diag_indices_from(K)]
+        data_vecs.append(2*k - S.T@d)
+
+        if np.ndim(data_vecs[-1]) == 1:
+            data_vecs[-1] = data_vecs[-1][:, None]
+        
+        # Normalize the data vector by its infinity norm
+        if normalize:
+            data_vecs[-1] /= np.max(np.abs(data_vecs[-1]))
+
+    return data_vecs if n_views > 1 else data_vecs[0]
